3e86c82f62f534f763a2f2c881eca29fb10f6bfd
Wallace Sousa <wallacerodrigon@gmail.com>
2019-03-16 23:24:43 -0300

    Implantar a primeira versão de produção

--- aplicativo/constants/ConstantesInternas.js
+++ aplicativo/constants/ConstantesInternas.js
@@ -7,8 +7,8 @@ export const TIPO_COMPARTILHAMENTO_MEDICACAO = 'CM';
 export const TIPO_COMPARTILHAMENTO_MEDICAMENTOS = 'RM';
 export const TIPO_COMPARTILHAMENTO_TRANSPORTE = 'TR';
 
-export const URL_BACKEND = "http://192.168.0.11:8080";
-export const URL_BACKEND_PRD = "http://sonecasaude.com.br";
+export const URL_BACKEND_DSV = "http://192.168.0.11:8080";
+export const URL_BACKEND = "http://walltec.net.br";
 export const NETWORK_ERROR = 'Error: Network Error';
 
 export const RETORNO_SUCESSO = 200;
